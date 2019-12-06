08fa1d3e34a40db1f75f66c3c1abe38516f459d3
yuanhang <yuanhang1@100tal.com>
2019-12-06 19:06:38 +0800

    删除

--- MLSUICoreDemo/Pods/Target Support Files/MLSUICore/MLSUICore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MLSUICore : NSObject
-@end
-@implementation PodsDummy_MLSUICore
-@end
--- MLSUICoreDemo/Pods/Target Support Files/Pods-MLSUICoreDemo/Pods-MLSUICoreDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MLSUICoreDemo : NSObject
-@end
-@implementation PodsDummy_Pods_MLSUICoreDemo
-@end
